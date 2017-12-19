c1753536ad86de38863045758297bc2fbb01d670
michal1590 <31709380+michal1590@users.noreply.github.com>
2017-12-19 17:16:10 +0100

    Cosmetical changes

--- MachineLearning/Linearregression/LinearRegressin.py
+++ MachineLearning/Linearregression/LinearRegressin.py
@@ -2,23 +2,37 @@
 """
 Created on Tue Dec  5 19:43:54 2017
 
+my implementation of Linear Regression with one variable
+
+
 @author: Michal
 """
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
+#%% some useful functions
 
 def computePrediction(X, theta):
+    """
+    compute y for a given X and theta: h=X*theta
+    """
     return np.dot(X, theta)
 
 
 
 def computeCost(X, y, theta):
+    """
+    compute 'cost' of fitting: J=((h-y)^2) / (2*m)
+    """
     return np.sum(((computePrediction(X,theta) - y) ** 2) / (2*m))
 
 
 def gradientDescent(X, y, theta, alpha, maxIterNum, treshold):
+    """
+    this function looks for best theta parameters, using gradient descent
+    """
     
     J_overTime = []
     J_new = computeCost(X, y, theta)
@@ -43,7 +57,7 @@ def gradientDescent(X, y, theta, alpha, maxIterNum, treshold):
 
 
 
-
+#%% loading and preprocessing data
 
 plt.close('all')
 
@@ -51,6 +65,7 @@ data = pd.read_csv(r'E:\Nauka\Machine Learning by Stanford University\week2\exer
                    header=None, names=['Population','Profit'])
 
 
+#data.dropna(axis='index',inplace=True)
 #data.plot.scatter('Population','Profit')
 #plt.title('raw data')
 #data = data.iloc[:10,:]
@@ -63,7 +78,10 @@ X = data.Population.values.reshape((m,n))
 y = data.Profit.values.reshape((m,1))
 
 X = np.insert(X, 0, np.ones(m), axis=1) #interception column
-n = n+1
+
+
+X, X_test, y, y_test = train_test_split(X,y)
+(m,n) = X.shape
 
 """
 #some test points
@@ -78,6 +96,9 @@ J = computeCost(X, y, theta)
 print('cost for theta =[-1,2]',J)
 """
 
+
+#%% fitting data and estimating best theta values
+
 #initialize fitting parameters
 theta = np.zeros((n,1))
 alpha = 0.01
@@ -90,8 +111,6 @@ treshold = 1e-11
 print('Best theta values are: ', theta)
 
 
-
-
 # Cost function over time visualization
 plt_J = plt.figure()
 plt.plot(list(range(numIter)),J_overTime)
@@ -101,7 +120,8 @@ plt.ylabel('value of cost function')
 
 
 
-#comparison to sklearn 
+
+#%%comparison to sklearn 
 
 from sklearn.linear_model import LinearRegression
 
@@ -113,8 +133,21 @@ thetaSKL = np.array([coef_intercept,coef[:,1:]])
 
 print('sklearn coefficients are: ', coef_intercept, ' and ', coef[:,1:])
 
-#Linear regression visualization
-data_lines = plt.figure()
+y_SKL = LR.predict(X_test)
+y_model = computePrediction(X_test, theta)
+
+#Mean Square Error
+MSE_SKL = np.sum((y_test-y_SKL)**2) / len(y_test)
+MSE_model = np.sum((y_test-y_model)**2) / len(y_test)
+
+print('MSE for sklearn prediction is {0:.4f}, when for my model MSE is {1:.4f}'.
+      format(MSE_SKL, MSE_model))
+
+
+
+#%% Visualization
+
+dataAndLines = plt.figure()
 feature = 1 # feature to scatter - col0 is an intercept column
 
 plt.title('train and test data')
@@ -124,14 +157,14 @@ plt.xlabel(data.columns[feature-1])
 
 #train and test data
 plt.scatter(X[:,feature],y,label='Train data', c='g', marker='o')
-X_test = np.transpose(X.mean(axis=0)).reshape(1,2) #generation of test data
+
 plt.scatter(X_test[:,feature], computePrediction(X_test,thetaSKL), 
             label='skelarn prediction', c='r', marker = '*', linewidths=5)
 plt.scatter(X_test[:,feature], computePrediction(X_test,theta), 
             label='model prediction', c='b', marker = 'x')
 
 #regression lines
-linesX = np.stack((X.min(axis=0),X.max(axis=0)),0)
+linesX = np.stack((X.min(axis=0),X.max(axis=0)),0) 
 plt.plot(linesX[:,feature], computePrediction(linesX,thetaSKL),label='skelarn regresion line',
          c='r', lw=5)
 
@@ -147,23 +180,21 @@ theta1 = np.linspace(-10,10,100)
 Jxx, Jyy = np.meshgrid(theta0, theta1)
 Z = np.ones(Jxx.shape)
 thetaTemp = np.zeros(theta.shape)
-Z_best = computeCost(X, y, theta)
+
 
 for i in range(len(theta0)):
     for j in range(len(theta1)):
         thetaTemp[0] = theta0[i]
         thetaTemp[1] = theta0[j]
         Z[i,j] = computeCost(X, y, thetaTemp)
-        if Z[i,j]<Z_best:
-            thetaBle = thetaTemp
-        
+
 
 
 
 Z = np.transpose(Z)
 
 J_cont = plt.figure()
-plt.contour(Jxx, Jyy, Z, np.logspace(-2,3,num=20))
+plt.contour(Jxx, Jyy, Z, np.logspace(-2,3,num=15))
 plt.scatter(theta[0],theta[1],marker='x', c='r', linewidths=50)
 plt.scatter(thetaSKL[0],thetaSKL[1],marker='*', c='k', linewidths=1)
 plt.xlabel('theta0')
